3311b94b94ae2d5f5d7a920592c74ea6c2a42d3a
Alexander Liman <liman324@yandex.ru>
2020-03-09 12:47:58 +0600

    Create 24LC.cpp

---
+++ 24LC.cpp
@@ -0,0 +1,29 @@
+#include <24LC.h>
+#include <Wire.h>
+
+EEPROM24LC::EEPROM24LC(byte addr){ 
+        ADDR = addr;
+        Wire.begin();   
+};
+
+void EEPROM24LC::writeWire(uint16_t num, byte data){
+        Wire.beginTransmission(ADDR);
+        Wire.write(num >> 8); 
+        Wire.write(num & 0xFF);
+        Wire.write(data); 
+        Wire.endTransmission();
+        delay(5);
+}
+
+byte EEPROM24LC::readWire(uint16_t num){
+        Wire.beginTransmission(ADDR); 
+        Wire.write(num >> 8); 
+        Wire.write(num & 0xFF); 
+        Wire.endTransmission();
+
+        Wire.beginTransmission(ADDR);
+        Wire.requestFrom(ADDR,1); 
+        byte value = Wire.read();
+        delay(5);
+        return value;
+}
