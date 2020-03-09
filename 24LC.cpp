c4b1dc9400a4a84c942a0d706dfd2647d6541e37
Alexander Liman <liman324@yandex.ru>
2020-03-09 15:07:16 +0600

    Update 24LC.cpp

--- 24LC.cpp
+++ 24LC.cpp
@@ -12,7 +12,7 @@ void EEPROM24LC::writeWire(uint16_t num, byte data){
         Wire.write(num & 0xFF);
         Wire.write(data); 
         Wire.endTransmission();
-        delay(5);
+        delay(1);
 }
 
 byte EEPROM24LC::readWire(uint16_t num){
@@ -24,6 +24,18 @@ byte EEPROM24LC::readWire(uint16_t num){
         Wire.beginTransmission(ADDR);
         Wire.requestFrom(ADDR,1); 
         byte value = Wire.read();
-        delay(5);
+        delay(1);
         return value;
+}
+
+void EEPROM24LC::writeWire64(uint16_t num, byte arr_data[], byte dl){
+        Wire.beginTransmission(ADDR);
+        Wire.write(num >> 8); 
+        Wire.write(num & 0xFF);
+        for(i=0;i<dl;i++){
+        Wire.write(arr_data[i]);
+        delay(1);
+}
+        Wire.endTransmission();
+        delay(5);
 }
